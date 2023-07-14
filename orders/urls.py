eacde1912b17d176111b68b6e7bd90657057e1eb
medazza <midazza27@gmail.com>
2023-07-14 17:35:00 +0100

    class OrderView  and OrderIdView using CBVs

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HelloOrderView.as_view(),name='hello_order'),
+    path('orders/',views.OrderView.as_view(),name='orders'),
+    path('order/<int:pk>/',views.OrderIdView.as_view(),name='order'),
+    path('update-status/<int:order_id>/',views.UpdateOrderStatusView.as_view(),name='update_order_status'),
+    path('user/<int:user_id>/orders',views.UserOrdersView.as_view(),name='users_orders'),
+    path('user/<int:user_id>/order/<int:order_id>/',views.UserOrderDetailView.as_view(),name='user_order_detail'),
 ]
