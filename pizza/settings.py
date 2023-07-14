9b67853dfcbc3f007468e8d49bd092617419e1fb
medazza <midazza27@gmail.com>
2023-07-14 12:22:04 +0100

    JWT Authentication with Djoser

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,15 +35,22 @@ INSTALLED_APPS = [
 
     #third_party
     'rest_framework',
+    'djoser',
 ]
 
 AUTH_USER_MODEL='authentication.User'
 
 REST_FRAMEWORK={
     "NON_FIELD_ERRORS_KEY":"error",
-    # "DEFAULT_AUTHENTICATION_CLASSES":(
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    #     )
+    "DEFAULT_AUTHENTICATION_CLASSES":(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 MIDDLEWARE = [
